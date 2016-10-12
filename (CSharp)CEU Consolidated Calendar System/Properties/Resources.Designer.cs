e35b2f0bd1dadb47e11c716f97b5fbc6c5bee9ae
AngeloDesktop <AngeloDesktop@ANGELODESKTOP>
2016-10-12 19:10:18 +0800

    Fixed Resources

--- (CSharp)CEU Consolidated Calendar System/Properties/Resources.Designer.cs
+++ (CSharp)CEU Consolidated Calendar System/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace _CSharp_CEU_Consolidated_Calendar_System.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon rocket {
+            get {
+                object obj = ResourceManager.GetObject("rocket", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
